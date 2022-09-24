6d5411d102750f43a88522720d17504ed8a624b1
rnakai <rnakai@student.42tokyo.jp>
2022-09-24 10:36:00 +0900

    診断機能埋め込み

--- hajimeyo/assessment/assessment.js
+++ hajimeyo/assessment/assessment.js
@@ -1,22 +1,22 @@
-'use strict'
-'use strict';
+"use strict";
+"use strict";
 const answers = [
-  '{userName}のいいところは声です。{userName}の特徴的な声は皆を惹きつけ、心に残ります。',
-  '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
-  '{userName}のいいところは情熱です。{userName}の情熱に周りの人は感化されます。',
-  '{userName}のいいところは厳しさです。{userName}の厳しさがものごとをいつも成功に導きます。',
-  '{userName}のいいところは知識です。博識な{userName}を多くの人が頼りにしています。',
-  '{userName}のいいところはユニークさです。{userName}だけのその特徴が皆を楽しくさせます。',
-  '{userName}のいいところは用心深さです。{userName}の洞察に、多くの人が助けられます。',
-  '{userName}のいいところは見た目です。内側から溢れ出る{userName}の良さに皆が気を惹かれます。',
-  '{userName}のいいところは決断力です。{userName}がする決断にいつも助けられる人がいます。',
-  '{userName}のいいところは思いやりです。{userName}に気をかけてもらった多くの人が感謝しています。',
-  '{userName}のいいところは感受性です。{userName}が感じたことに皆が共感し、わかりあうことができます。',
-  '{userName}のいいところは節度です。強引すぎない{userName}の考えに皆が感謝しています。',
-  '{userName}のいいところは好奇心です。新しいことに向かっていく{userName}の心構えが多くの人に魅力的に映ります。',
-  '{userName}のいいところは気配りです。{userName}の配慮が多くの人を救っています。',
-  '{userName}のいいところはその全てです。ありのままの{userName}自身がいいところなのです。',
-  '{userName}のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる{userName}が皆から評価されています。'
+  "{userName}のいいところは声です。{userName}の特徴的な声は皆を惹きつけ、心に残ります。",
+  "{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。",
+  "{userName}のいいところは情熱です。{userName}の情熱に周りの人は感化されます。",
+  "{userName}のいいところは厳しさです。{userName}の厳しさがものごとをいつも成功に導きます。",
+  "{userName}のいいところは知識です。博識な{userName}を多くの人が頼りにしています。",
+  "{userName}のいいところはユニークさです。{userName}だけのその特徴が皆を楽しくさせます。",
+  "{userName}のいいところは用心深さです。{userName}の洞察に、多くの人が助けられます。",
+  "{userName}のいいところは見た目です。内側から溢れ出る{userName}の良さに皆が気を惹かれます。",
+  "{userName}のいいところは決断力です。{userName}がする決断にいつも助けられる人がいます。",
+  "{userName}のいいところは思いやりです。{userName}に気をかけてもらった多くの人が感謝しています。",
+  "{userName}のいいところは感受性です。{userName}が感じたことに皆が共感し、わかりあうことができます。",
+  "{userName}のいいところは節度です。強引すぎない{userName}の考えに皆が感謝しています。",
+  "{userName}のいいところは好奇心です。新しいことに向かっていく{userName}の心構えが多くの人に魅力的に映ります。",
+  "{userName}のいいところは気配りです。{userName}の配慮が多くの人を救っています。",
+  "{userName}のいいところはその全てです。ありのままの{userName}自身がいいところなのです。",
+  "{userName}のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる{userName}が皆から評価されています。",
 ];
 
 /**
@@ -30,7 +30,28 @@ function assessment(userName) {
     sumOfUserCharCode += userName.charCodeAt(i);
   }
 
-  const index = sumOfUserCharCode % answers.length
+  const index = sumOfUserCharCode % answers.length;
   const result = answers[index];
-  return result.replaceAll('{userName}', userName);
+  return result.replaceAll("{userName}", userName);
 }
+
+function main() {
+  const assessButton = document.getElementById("assessment");
+
+  assessButton.onclick = () => {
+    const userName = document.getElementById("user-name").value;
+    if (userName.length === 0) {
+      return ;
+    }
+    // clear element
+    const assessmentArea = document.getElementById("assessmentArea");
+    assessmentArea.innerHTML = '';
+
+    // append element
+    const assessmentParagraph = document.createElement("p");
+    assessmentParagraph.innerText = assessment(userName);
+    assessmentArea.appendChild(assessmentParagraph);
+  };
+}
+
+main();
