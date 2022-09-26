dfc16a66b30f16273d9636cfb3234d1b568c9665
rnakai <rnakai@student.42tokyo.jp>
2022-09-26 18:23:04 +0900

    メモ化アルゴリズム

--- nodejs-study/app.js
+++ nodejs-study/app.js
@@ -1,13 +1,24 @@
 "use strict";
-function fib(num) {
+function fib2(num) {
+  const memory = new Map();
+  return _fib(num, memory);
+}
+
+function _fib(num, memory) {
   if (num == 0) {
     return 0;
   } else if (num == 1) {
     return 1;
   }
-  return fib(num - 1) + fib(num - 2);
+  const cache = memory.get(num)
+  if (cache) {
+    return cache
+  }
+  const result = fib(num - 1, memory) + fib(num - 2, memory);
+  memory.set(num, result);
+  return result;
 }
 
-for (let i = 0; i < 40; ++i) {
-  console.log(fib(i));
+for (let i = 0; i < 100; ++i) {
+  console.log(fib2(i));
 }
